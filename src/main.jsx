dc9004d5039452fee765dd4aa8f3ed593c7ebb87
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2022-08-17 01:38:02 +0800

    feat: add react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import RQComponent from "./RQComponent";
+import SWRComponent from "./SWRComponent";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <h1>Hello, world</h1>
+              <h1>
+                Look at the difference of data fetching between swr and
+                react-query
+              </h1>
+              <div
+                style={{
+                  display: "flex",
+                  gap: "1rem",
+                }}
+              >
+                <Link to="/swr">SWR</Link>
+                <Link to="/react-query">React Query</Link>
+              </div>
+            </div>
+          }
+        />
+        <Route path="/swr" element={<SWRComponent />} />
+        <Route path="/react-query" element={<RQComponent />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
