93f2383850003cc38d1601695ff6b72b2343a931
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2022-08-17 01:39:24 +0800

    feat(pages): add SWRComponent for comparison against react-query

---
+++ src/SWRComponent.jsx
@@ -0,0 +1,32 @@
+import { fetcher } from "./utils";
+import useSWR from "swr";
+import { Link } from "react-router-dom";
+const fetchPosts = async () => {
+  const rand = Math.floor(Math.random() * 100);
+  const res = await fetch(
+    "https://jsonplaceholder.typicode.com/posts/" + rand.toString()
+  );
+  return res.json();
+};
+const SWRComponent = () => {
+  const { data, error, isValidating, mutate } = useSWR("posts", fetchPosts);
+  return (
+    <div>
+      <button onClick={() => mutate()}>Refetch random post</button>
+      <Link to="/">
+        <button>Back to index</button>
+      </Link>
+      {isValidating ? <div>Rechecking...</div> : ""}
+      {data && !isValidating ? (
+        <div key={data.id}>
+          <h1>{data.title}</h1>
+          <p>{data.body}</p>
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+};
+
+export default SWRComponent;
